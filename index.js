44993f8169e7a70b1328b5fc2dffbf19111e82c6
Brenda-mwaura <wairimumwaura2020@gmail.com>
2023-01-16 09:46:59 +0300

    updated code

--- index.js
+++ index.js
@@ -1,27 +1,37 @@
-const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "9e790d57fbmsh88aa75452dcb4f5p14b21ajsn56f797d7f807",
-    "X-RapidAPI-Host": "Page-App.p.rapidapi.com",
-  },
+const contentContainer = document.querySelector(".content-container");
+
+const fetchData = () => {
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "854c4ff541msheb75876113b7b0cp10dd56jsn2596df1cde05",
+      "X-RapidAPI-Host": "bb-finance.p.rapidapi.com",
+    },
+  };
+
+  fetch(
+    "https://bb-finance.p.rapidapi.com/market/auto-complete?query=apple",
+    options
+  )
+    .then((response) => response.json())
+    .then((response) => {
+      const news = response.news
+        .map((item) => {
+          const { title, thumbnailImage, longURL } = item;
+
+          return `
+         <div class="card">
+            <h1>${title}</h1>
+            <img src="${thumbnailImage}" alt="" />
+            <a href="${longURL}">go to website</a>
+        </div>
+        `;
+        })
+        .join("");
+
+      contentContainer.innerHTML = news;
+    })
+    .catch((err) => console.error(err));
 };
 
-fetch("https://Page-App.p.rapidapi.com/PageApp?limit=10&page=0", options)
-  .then((response) => response.json())
-  .then(response)=> 
-    const PageApp =response.Search;
-    PageApp.map (PageApp=>{
-
-      // console.log(PageApp.Title);  
-
-   const name= PageApp.Title;
-    const email =PageApp. email
-    const year= PageApp.year 
-  
-   const Homepage= <li> <img src="$ {Title}" alt="${email}"> <h3>${year}</h3> <P> ${year}
-</p></li>; 
-document.querySelector(".Page App").innerHTML +=Homepage;
-
-    })
-
-  .catch(err => console.error(err));
+fetchData();
